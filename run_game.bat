c9b1bfdcf5de8a2d9dfe0e9ee0647d2ff6636143
Cody Heffner <cody.heffner@gmail.com>
2017-11-21 21:10:08 -0700

    restructure

---
+++ run_game.bat
@@ -0,0 +1,1 @@
+halite.exe -d "240 160" "python src/MyBot.py" "python src/MyBot.py" "python src/MyBot.py" "python src/Settler.py"
---
+++ run_game.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+./halite -d "240 160" "python3 src/MyBot.py" "python3 src/Settler.py"
